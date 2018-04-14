b50330d99c0bc4cf8e9f7790a0367acc4f6062a0
Vivek Kumar <vvkkumar06@gmail.com>
2018-04-14 14:40:21 +0530

    widgets location relative to pane added

--- drag.js
+++ drag.js
@@ -1,6 +1,42 @@
+/**
+ *  Since onMove: dragMoveListener we are using translate function which actually translate an element
+ *  from  one position to anoother, which is not actually original location of an element in HTML body, If 
+ *  an element starts from one position its value will be 0, and we are translating according to the data 
+ *  it moved. We don't want to do this. 
+ *  We have many panes we want to keep location of panes relative to HTML Body and we want to keep our 
+ *  draggables(or widgets) relative to panes( or dropzone). 
+ *  In this case what I am trying to do here is, I will save ID of pane in which widget is currently residing.
+ *  and location of widgets relative to panes.
+ */
 
 var draggedOut = false;
 var startPos = {x: 0,y: 0};
+
+//starting positions of widgets and pane
+var widget = [
+            {
+                id:'widget-1',
+                parentId: 'pane-1',
+                x: 9,
+                y: 83
+            },
+            {
+                id:'widget-2',
+                parentId: 'pane-2',
+                x: 13.25,
+                y: 223.8125
+            }
+        ]
+var pane = [{ 
+                 id: 'pane-1',
+                 x: 45.703125,
+                 y: -72.125
+            },
+            {
+                 id: 'pane-2',
+                 x: 45.703125,
+                 y: 339.875
+            }]
 // target elements with the "draggable" class
  interact('.draggable')
         .draggable({
@@ -8,7 +44,7 @@ var startPos = {x: 0,y: 0};
      inertia: false,
      // keep the element within the area of it's parent
      restrict: {
-         restriction: ".container",
+         restriction: '.container',
          endOnly: true,
          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
      },
@@ -20,14 +56,12 @@ var startPos = {x: 0,y: 0};
              startPos.x = event.target.getAttribute('data-x') || 0;
              startPos.y = event.target.getAttribute('data-y') || 0;
          }
-         console.log(startPos.x,startPos.y);
      },
 
      // call this function on every dragmove event
      onmove: dragMoveListener,
      // call this function on every dragend event
      onend: function (event) {
-    
      }
     });
     /* The dragging code for '.draggable' from the demo above
@@ -62,12 +96,17 @@ var startPos = {x: 0,y: 0};
         event.relatedTarget.classList.add('can-not-drop');
         event.relatedTarget.textContent = 'Dragged out';
         draggedOut = true;
-        console.log(draggedOut);
     },
     ondrop: function (event) {
         event.relatedTarget.textContent = 'Dropped';
+        dropzoneID = event.target.getAttribute('id');
         draggedOut = false;
-        console.log(draggedOut);
+        //Use this to store updated location of widgets
+        widget[0].x = event.relatedTarget.getBoundingClientRect().x - event.target.getBoundingClientRect().x;
+        widget[0].y = event.relatedTarget.getBoundingClientRect().y - event.target.getBoundingClientRect().y;
+          //TODO just for testing
+        console.log(widget[0].x, widget[0].y); 
+        console.log(dropzoneID);
     },
     ondropdeactivate: function (event) {
         // remove active dropzone feedback
